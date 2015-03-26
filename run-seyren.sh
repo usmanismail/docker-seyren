6e306c7f7fbd844c1cc665e64d18fb9e6c60d824
Bryan Shell <bryan.shell@orbitz.com>
2015-03-26 10:19:29 -0500

    Enable GRAPHITE_URL to be supplied from the environment

--- run-seyren.sh
+++ run-seyren.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 set -o errexit
 
-if [ $# -ne 1 ]
-  then
-    echo "Usage: run-seyren.sh GRAPHITE_URL"
+if [ -z ${GRAPHITE_URL} ]
+   then
+       if [ $# -ne 1 ]
+       then
+           echo "Usage: run-seyren.sh GRAPHITE_URL"
+       fi
+       export GRAPHITE_URL=$1
 fi
 
-export GRAPHITE_URL=$1
 export MONGO_URL=mongodb://$MONGODB_PORT_27017_TCP_ADDR:$MONGODB_PORT_27017_TCP_PORT/seyren
 
 echo "Graphite URL $GRAPHITE_URL"
